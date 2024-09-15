b70aa10b823c281de08d3768c78cc103fcd1f9bc
Adityakapadne08 <142308808+Adityakapadne08@users.noreply.github.com>
2024-09-15 23:12:10 +0530

    Added slide over for MobileNav

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import MobileNav from "./MobileNav";
 
 const Header = () => {
   return (
@@ -14,6 +15,9 @@ const Header = () => {
         >
           AdityaDelivers.com
         </Link>
+        <div className="md:hidden">
+            <MobileNav/>
+        </div>
       </div>
     </div>
   );
