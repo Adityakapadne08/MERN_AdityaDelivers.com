ffb1f3e4753cc34f36ee4ec55740a23d56e5470f
Adityakapadne08 <142308808+Adityakapadne08@users.noreply.github.com>
2024-09-15 18:28:29 +0530

    setup project and basic of a server route

---
+++ backend/src/index.ts
@@ -0,0 +1,26 @@
+import express,{Request,Response} from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+//import mongoose from 'mongoose';
+//import myUserRoute from "./routes/MyUserRoutes"
+
+// mongoose
+// .connect(process.env.MONGODB_CONNECTION_STRING as string)
+// .then(()=>
+// console.log('connected to Database'));
+
+const app = express();
+app.use(express.json())
+app.use(cors())
+
+app.get("/test", async(req:Request, res:Response)=>{
+res.json({message:"Hello!"});
+});
+
+
+//app.post("/api/my/user", myUserRoute)
+
+const PORT = 7000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
