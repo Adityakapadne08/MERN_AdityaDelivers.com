a6c59b9127bd7926f009d1ee84f6f29ab279e87a
Adityakapadne08 <142308808+Adityakapadne08@users.noreply.github.com>
2024-09-15 22:16:27 +0530

    Adding layout and Header

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -1,12 +1,13 @@
 import { Navigate, Route, Routes } from "react-router-dom"
+import Layout from "./layouts/layout"
 
 const AppRoutes = ()=>{
     return (
         <Routes>
-            <Route path = "/" element = {<span>HOME PAGE</span>}/>
+            <Route path = "/" element = {<Layout>HomePage</Layout>}/>
             <Route path = "/user-profile" element = {<span>USER PROFILE</span>}/>
             <Route path = "*" element ={ <Navigate to ="/"/>}/>
         </Routes>
     )
 }
-export default AppRoutes
+export default AppRoutes;
