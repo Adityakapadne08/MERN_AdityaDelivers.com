b65387cef161f5d6f1860737dab8b91212159f40
Adityakapadne08 <142308808+Adityakapadne08@users.noreply.github.com>
2024-09-19 15:20:15 +0530

    User profile form

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -1,16 +1,31 @@
-import { Navigate, Route, Routes } from "react-router-dom"
-import Layout from "./layouts/layout"
+import { Navigate, Route, Routes } from "react-router-dom";
+import Layout from "./layouts/layout";
 import HomePage from "./pages/HomePage";
 import AuthCallbackPage from "./auth/AuthCallbackPage";
+import UserProfilePage from "./pages/UserProfilePage";
 
-const AppRoutes = ()=>{
-    return (
-        <Routes>
-            <Route path = "/auth-callback" element = {<AuthCallbackPage/>}/>
-            <Route path = "/" element = {<Layout><HomePage/></Layout>}/>
-            <Route path = "/user-profile" element = {<span>USER PROFILE</span>}/>
-            <Route path = "*" element ={ <Navigate to ="/"/>}/>
-        </Routes>
-    )
-}
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/auth-callback" element={<AuthCallbackPage />} />
+      <Route
+        path="/"
+        element={
+          <Layout showHero>
+            <HomePage />
+          </Layout>
+        }
+      />
+      <Route
+        path="/user-profile"
+        element={
+          <Layout>
+            <UserProfilePage />
+          </Layout>
+        }
+      />
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  );
+};
 export default AppRoutes;
