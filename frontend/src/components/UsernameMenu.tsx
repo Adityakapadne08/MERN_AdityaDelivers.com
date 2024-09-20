9d418d0dac882b887f0137abc16e6cde0f52ce12
Adityakapadne08 <142308808+Adityakapadne08@users.noreply.github.com>
2024-09-21 03:14:42 +0530

    Create restaurant Form

--- frontend/src/components/UsernameMenu.tsx
+++ frontend/src/components/UsernameMenu.tsx
@@ -19,6 +19,11 @@ const UsernameMenu = () => {
         {user?.email}
       </DropdownMenuTrigger>
       <DropdownMenuContent>
+      <DropdownMenuItem>
+          <Link to="/manage-restaurant" className="font-bold hover:text-orange-500">
+        Manage Restaurant
+          </Link>
+        </DropdownMenuItem>
         <DropdownMenuItem>
           <Link to="/user-profile" className="font-bold hover:text-orange-500">
             User Profile
@@ -26,7 +31,10 @@ const UsernameMenu = () => {
         </DropdownMenuItem>
         <Separator />
         <DropdownMenuItem>
-          <Button onClick={()=>logout()} className="flex flex-1 font-bold bg-orange-500">
+          <Button
+            onClick={() => logout()}
+            className="flex flex-1 font-bold bg-orange-500"
+          >
             Log Out
           </Button>
         </DropdownMenuItem>
---
+++ frontend/src/pages/ManageRestaurantPage.tsx
@@ -0,0 +1,10 @@
+import ManageRestaurantForm from "@/forms/manage-restaurants-form/ManageRestaurantForm"
+
+
+const ManageRestaurantPage = () => {
+  return (
+  <ManageRestaurantForm/>
+  )
+}
+
+export default ManageRestaurantPage
